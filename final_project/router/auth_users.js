9d37baa013eedfeaf8d2f501fba866104410863a
MantCib3 <mattaiton567123@gmail.com>
2025-03-04 14:18:17 +1000

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,26 +5,54 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username)=>{
+  let userwithsamename = users.filter((user)=>{
+    return user.username === username
+  });
+  if(userwithsamename.length > 0){
+    return true;
+  } else {
+    return false;
+  }
 }
-
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
-
+const authenticatedUser = (username,password)=>{
+const user = users.find(user => user.username === username)
+return user && user.password === password; 
+};
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  if (!username || !password) {
+        return res.status(404).json({message: "Error logging in"});
+    }
+    if (authenticatedUser(username,password)) {
+      let accessToken = jwt.sign({
+          data: password
+        }, 'access', { expiresIn: 60 * 20 });
+        req.session.authorization = {
+          accessToken, username
+      }
+      return res.status(200).send("Customer successfully logged in");
+    } else {
+      return res.status(208).send("Incorrect Login. Check credentials");
+    }
 });
-
-// Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const book = books.find(b => b.isbn === isbn);
+   if (!book){
+    return res.status(404).json({message:"Book not found."})
+   }
+   const { reviewer, comment } = req.body
+    if (!reviewer||!comment){
+        return res.status(400).json({message:"User and comment required"})
+    }
+    
+    book.reviews[reviewer] = comment;
+    return res.status(200).json({message:"Review added successfully", review:book.reviews});
 });
-
+                                                                            
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
